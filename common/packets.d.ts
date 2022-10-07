4f026d9fdd8e50014ac349b2cb0ea72162308ead
metamuffin <metamuffin@disroot.org>
2022-10-07 23:18:58 +0200

    revert back to bad types

--- common/packets.d.ts
+++ common/packets.d.ts
@@ -13,31 +13,34 @@ interface F_RTCIceCandidateInit {
     usernameFragment?: string | null
 }
 
-export type ClientboundPacket =
-    { init: { your_id: number, version: string } }
-    | { client_join: { id: number } } // join: more like "appear" - also sent when you join for others that were there before you.
-    | { client_leave: { id: number } }
-    | { message: { sender: number, message: string /* encrypted RelayMessageWrapper */ } }
+export interface ClientboundPacket {
+    init?: { your_id: number, version: string }
+    client_join?: { id: number }  // join: more like "appear" - also sent when you join for others that were there before you.
+    client_leave?: { id: number }
+    message?: { sender: number, message: string /* encrypted RelayMessageWrapper */ }
+}
 
-export type ServerboundPacket =
-    { ping: null }
-    | { relay: { recipient?: number, message: string /* encrypted RelayMessageWrapper */ } }
+export interface ServerboundPacket {
+    ping?: null
+    relay?: { recipient?: number, message: string /* encrypted RelayMessageWrapper */ }
+}
 
 export interface RelayMessageWrapper {
     sender: number, // redundancy to ensure the server didn't cheat
     inner: RelayMessage
 }
 
-export type RelayMessage =
-    { chat: ChatMessage }
-    | { identify: { username: string } }
-    | { provide: ProvideInfo }
-    | { request: { id: string } }
-    | { provide_stop: { id: string } }
-    | { request_stop: { id: string } }
-    | { offer: Sdp }
-    | { answer: Sdp }
-    | { ice_candidate: F_RTCIceCandidateInit }
+export interface RelayMessage {
+    chat?: ChatMessage
+    identify?: { username: string }
+    provide?: ProvideInfo
+    request?: { id: string }
+    provide_stop?: { id: string }
+    request_stop?: { id: string }
+    offer?: Sdp
+    answer?: Sdp
+    ice_candidate?: F_RTCIceCandidateInit
+}
 
 export interface ChatMessage { text?: string, image?: string }
 export type ResourceKind = "track" | "file"
